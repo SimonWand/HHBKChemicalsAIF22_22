1c4a62e18ecfa085d2cda2a2ab46c4edf4d00989
Simon Buczynski <simonbuczynski@gmail.com>
2022-11-06 16:50:48 +0100

    knopf weg

--- HHBKChemicalsAIF22_22/Simon.Designer.cs
+++ HHBKChemicalsAIF22_22/Simon.Designer.cs
@@ -28,24 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(372, 206);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "hiii";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // Simon
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
             this.Name = "Simon";
             this.Text = "Simon";
             this.ResumeLayout(false);
@@ -53,7 +42,5 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
